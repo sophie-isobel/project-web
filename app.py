9458200a1275a6d9b131018d219f160bc6c36c2c
Sophie McMurray <sophie.mcmurray@decoded.com>
2022-09-22 11:26:54 +0100

    Add updated code for dynamic recommendations

--- app.py
+++ app.py
@@ -9,10 +9,12 @@ app = Flask(__name__)
 @app.route("/")
 def movie():
     with sqlite3.connect("db/movie.db") as c:
-        return render_template('index.html', suggests=c.execute("""
+        return render_template('index.html', 
+           api_url="http://127.0.0.1:8000", 
+           suggests=c.execute("""
             SELECT ItemID, MovieTitle, ROUND(Rating, 1) FROM movies 
             ORDER BY Rating DESC
-            LIMIT 5 
+            LIMIT 10 
         """))
 
 @app.route("/sample")
