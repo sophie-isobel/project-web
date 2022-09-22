51b7f704a13f391626ce9366ca2eba4fdc21e667
Sophie McMurray <sophie.mcmurray@decoded.com>
2022-09-22 10:51:30 +0100

    Add updated files and database

--- app.py
+++ app.py
@@ -8,4 +8,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def movie():
-    return render_template('index.html')
+    with sqlite3.connect("db/movie.db") as c:
+        return render_template('index.html', suggests=c.execute("""
+            SELECT ItemID, MovieTitle, ROUND(Rating, 1) FROM movies 
+            ORDER BY Rating DESC
+            LIMIT 10 
+        """))
+
+@app.route("/sample")
+def movie_sample():
+    return render_template('index.html', suggests=[(1, "FilmA", 3.5), (2, "FilmB", 4.5)])
